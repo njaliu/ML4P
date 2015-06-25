11a471759ca1603234d65ecae7fce4ef04f57469
Ting Su <tsuletgo@gmail.com>
2015-06-25 00:07:50 -0700

    Update the file to calculate precision with variable reusing

--- NamePrediction/aliu-test/sm_test.js
+++ NamePrediction/aliu-test/sm_test.js
@@ -1,24 +1,37 @@
 var sourceMap = require('source-map');
+var fs = require('fs');
 
 var rawSourceMap = {
   version: 3,
-  file: '/home/aliu/Research/closure-compiler/aliu-test/test.min.js',
+  file: 'aliu-test/test1.map',
   names: ["createGraphRepresentation","g","console","log","repr","nodes","edges","N","length","i","node","newnode","id","label","x","Math","cos","PI","y","sin","size","color","edge","newedge","source","target"],
-  sources: ['/home/aliu/Research/closure-compiler/aliu-test/test.js'],
+  sources: ['aliu-test/test1.js'],
+  sourceRoot: '/home/aliu/Research/closure-compiler/',
   mappings: 'AAAAA,QAASA,0BAAyB,CAACC,CAAD,CAAI,CACpCC,OAAAC,IAAA,CAAYF,CAAZ,CACA,KAAIG,EAAO,CAAEC,MAAM,EAAR,CAAYC,MAAM,EAAlB,CAAX,CACIC,EAAIN,CAAAI,MAAAG,OACR,KAAKC,CAAL,GAAUR,EAAAI,MAAV,CAAmB,CACjB,IAAIK,EAAOT,CAAAI,MAAA,CAAQI,CAAR,CAAX,CACIE,EAAU,CACVC,GAAIF,CAAAE,GADM,CAEVC,MAAOH,CAAAG,MAFG,CAGVC,EAAG,GAAHA,CAASC,IAAAC,IAAA,CAAS,CAAT,CAAaP,CAAb,CAAiBM,IAAAE,GAAjB,CAA2BV,CAA3B,CAHC,CAIVW,EAAG,GAAHA,CAASH,IAAAI,IAAA,CAAS,CAAT,CAAaV,CAAb,CAAiBM,IAAAE,GAAjB,CAA2BV,CAA3B,CAJC,CAKVa,KAAM,CALI,CAMVC,MAAOX,CAAAW,MANG,CAQdjB,EAAAC,MAAA,CAAWI,CAAX,CAAA,CAAgBE,CAVC,CAanB,IAAKF,CAAL,GAAUR,EAAAK,MAAV,CACMgB,CAOJ,CAPWrB,CAAAK,MAAA,CAAQG,CAAR,CAOX,CAAAL,CAAAE,MAAA,CAAWG,CAAX,CAAA,CANcc,CACVX,GAAIU,CAAAV,GADMW,CAEVC,OAAQF,CAAAE,OAFED,CAGVE,OAAQH,CAAAG,OAHEF,CAIVV,MAAOS,CAAAT,MAJGU,CAShB,OAAOnB,EA5B6B;'
 };
 
-var smc = new sourceMap.SourceMapConsumer(rawSourceMap);
+var dir_base = '/home/aliu/Research/closure-compiler/';
+var rawSourceMap_1 = JSON.parse(fs.readFileSync('/home/aliu/Research/closure-compiler/aliu-test/test1.map','utf-8'));
+//console.log(rawSourceMap_1.file);
+//rawSourceMap_1.file = dir_base + rawSourceMap_1.file;
+//console.log(rawSourceMap_1.sources);
+//rawSourceMap_1.sources = "['/home/aliu/Research/closure-compiler/aliu-test/test1.js']" ;
+//console.log(rawSourceMap_1.sources);
+//console.log(rawSourceMap_1);
+rawSourceMap_1["sourceRoot"] = dir_base;
+
+
+var smc = new sourceMap.SourceMapConsumer(rawSourceMap_1);
 
 console.log(smc.sources);
 
 console.log(smc.originalPositionFor({
   line: 1,
-  column: 268
+  column: 80
 }));
 
 console.log(smc.generatedPositionFor({
-  source: '/home/aliu/Research/closure-compiler/aliu-test/test.js',
+  source: '/home/aliu/Research/closure-compiler/aliu-test/test1.js',
   line: 19,
   column: 11
 }));
---
+++ NamePrediction/src/calculate.js
@@ -0,0 +1,132 @@
+var UglifyJS = require("uglify-js");
+var fs = require("fs");
+var sourceMap = require('source-map');
+
+/*
+	origin - JSON: variable table of the original file
+	minified - JSON: variable mapping table in the minified file
+	predicted - JSON: variable table of the predicted file 
+*/
+function cal_precision(origin, minified, predicted) {
+	var total = origin.length;
+	var count = 0;
+
+
+	for(var i = 0; i < predicted.length; i++) {
+		var o_var = minified[i];
+		var p_var = predicted[i];
+
+		console.log("## o_var: " + o_var + ", p_var: " + p_var);
+		if(o_var == p_var)
+			count++;
+	}
+
+	console.log("\n## Correct: " + count + ", Total: " + total);
+
+	if(total != 0)
+		return (count / total).toFixed(2);
+	else
+		return 1;
+}
+
+
+
+//extract the variable table of the input file
+function extract_variables(code) {
+	var varTab = [];
+	var top_level = UglifyJS.parse(code);
+
+	var walker = new UglifyJS.TreeWalker(function(node){
+    	if (node instanceof UglifyJS.AST_VarDef) {
+      	  	// string_template is a cute little function that UglifyJS uses for warnings
+       	 	console.log(UglifyJS.string_template("Found Variable {name} at {line},{col}", {
+       	     	name: node.name.name,
+       	     	line: node.start.line,
+      	      	col: node.start.col
+      	  	}));
+       	 	varTab.push(node.name.name);
+   		}
+    	else if (node instanceof UglifyJS.AST_SymbolFunarg) {
+        	// string_template is a cute little function that UglifyJS uses for warnings
+        	console.log(UglifyJS.string_template("Found Function Argument {name} at {line},{col}", {
+            	name: node.name,
+            	line: node.start.line,
+            	col: node.start.col
+        	}));
+        	varTab.push(node.name);
+    	}
+	});
+
+	top_level.walk(walker);
+	//console.log("hahaha " + varTab);
+	
+	return varTab;
+}
+
+//extract the variable mapping from the minified file and source-map file
+// code: minified file
+// rawSourceMap: source-map file
+function extract_mapping(code, rawSourceMap) {
+	var varTab = [];
+	var mapTab = []
+	var top_level = UglifyJS.parse(code);
+	var smc = new sourceMap.SourceMapConsumer(rawSourceMap)
+
+	var walker = new UglifyJS.TreeWalker(function(node){
+    	if (node instanceof UglifyJS.AST_VarDef) {
+      	  	// string_template is a cute little function that UglifyJS uses for warnings
+       	 	console.log(UglifyJS.string_template("Found Variable {name} at {line},{col}", {
+       	     	name: node.name.name,
+       	     	line: node.start.line,
+      	      	col: node.start.col
+      	  	}));
+       	 	varTab.push({"name": node.name.name, "line": node.start.line, "col": node.start.col});
+   		}
+    	else if (node instanceof UglifyJS.AST_SymbolFunarg) {
+        	// string_template is a cute little function that UglifyJS uses for warnings
+        	console.log(UglifyJS.string_template("Found Function Argument {name} at {line},{col}", {
+            	name: node.name,
+            	line: node.start.line,
+            	col: node.start.col
+        	}));
+        	varTab.push({"name": node.name, "line": node.start.line, "col": node.start.col});
+    	}
+	});
+
+	top_level.walk(walker);
+
+	for( var i = 0; i < varTab.length; i++ ) {
+		var v = varTab[i];
+		mapTab.push((smc.originalPositionFor({line: v.line, column: v.col})).name);
+	}
+
+	//console.log("hahaha " + varTab + ", " + varTab.length);
+	//console.log("hahaha " + mapTab);
+
+	return mapTab;
+}
+
+function test_main() {
+	var dir_base = '/home/aliu/Research/closure-compiler/';
+	var mapFile =  dir_base + 'aliu-test/test11.map'
+	var rawSourceMap = JSON.parse(fs.readFileSync(mapFile,'utf-8'));
+	rawSourceMap["sourceRoot"] = dir_base;
+
+	var minified_file = dir_base + 'aliu-test/test11.min.js';
+	var minified_code = fs.readFileSync(minified_file, 'utf-8');
+	var mapTab = extract_mapping(minified_code, rawSourceMap);
+
+	var predicted_file = dir_base + 'aliu-test/test11.rename.js';
+	var predicted_code = fs.readFileSync(predicted_file, 'utf-8');
+	var varTab = extract_variables(predicted_code);
+
+	var origin_file = dir_base + 'aliu-test/test11.js';
+	var origin_code = fs.readFileSync(origin_file, 'utf-8');
+	var origin_varTab = extract_variables(origin_code);
+	//console.log(origin_code);
+	//console.log(origin_varTab.length);
+
+	console.log("\n## Precision: " + cal_precision(origin_varTab, mapTab, varTab));
+}
+
+test_main();
